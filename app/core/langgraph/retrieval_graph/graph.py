7863ae3597ffbf5985055e280dca1b8cd33215df
Tich <tich@exokames.com>
2025-05-22 11:24:03 +0200

    wip

--- app/core/langgraph/retrieval_graph/graph.py
+++ app/core/langgraph/retrieval_graph/graph.py
@@ -291,11 +291,6 @@ class LangGraphAgent:
         """
         config = {
             "configurable": {"thread_id": session_id},
-            "callbacks": [
-                CallbackHandler(
-                    environment=settings.ENVIRONMENT.value, debug=False, user_id=user_id, session_id=session_id
-                )
-            ],
         }
         if self._graph is None:
             self._graph = await self.create_graph()
